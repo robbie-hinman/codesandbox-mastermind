99049fd5e921a03031eeb1be58e06e09b28c0186
Robbie Hinman <robbie.hinman@gmail.com>
2022-12-28 23:24:53 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import CodeGuess from "./CodeGuess";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Mastermind</h1>
       <CodeGuess>
         <ColorPin color="blue" />
         <ColorPin color="red" />
